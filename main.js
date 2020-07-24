cbb66fe5179cbbddbcf214633c00fce19aa53d0c
park seonju <cucu9823@naver.com>
2020-07-24 12:55:25 +0900

    07.24

--- main.js
+++ main.js
@@ -4,46 +4,24 @@ let getID = JSON.parse(id_LS);
 for (var i = 0; i < id.length; i++) {
   id[i].innerText = getID.id;
 }
-
-var change1 = document.querySelector("#change1");
-var change2 = document.querySelector("#change2");
-change1.addEventListener("click", function () {
-  if (change1.style.backgroundColor != "rgb(178,235,244)") {
-    change1.style.backgroundColor = "rgb(178,235,244)";  //178 흰색
-  }
-  else {
-    change1.style.backgroundColor = "rgb(0,149,249)";
-  }
-});
-change2.addEventListener("click", function () {
-  if (change2.style.backgroundColor != "rgb(0,149,249)") {
-    change2.style.backgroundColor = "rgb(178,235,244)";
-  } else {
-    change2.style.backgroundColor = "rgb(0,149,249)";
-  }
-});
 var home = document.querySelector(".icon-user");
+var main = document.querySelector(".icon-home");
+var insta = document.querySelector(".gridinsta");
+
 var modals = document.querySelector(".modal");
-// Modal을 띄우는 클래스 이름을 가져옵니다.
 var btns = document.querySelectorAll(".modal_open_btn");
-// Modal을 닫는 close 클래스를 가져옵니다.
 var cancels = document.querySelector("#cancel");
 var body = document.querySelector("body");
 var none = "showing";
-// 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의합니다.
 for (var i = 0; i < btns.length; i++) {
-  // 해당 클래스의 내용을 클릭하면 Modal을 띄웁니다.
   btns[i].addEventListener("click", function () {
     modals.classList.remove(none);
     body.style.overflow = "hidden";
   });
-  // <span> 태그(X 버튼)를 클릭하면 Modal이 닫습니다.
-
   cancels.addEventListener("click", function () {
     modals.classList.add(none);
     body.style.overflow = "auto";
   });
-  // Modal 영역 밖을 클릭하면 Modal을 닫습니다.
   window.onclick = function (event) {
     if (event.target.className == "modal") {
       modals.classList.add(none);
@@ -53,4 +31,39 @@ for (var i = 0; i < btns.length; i++) {
 }
 home.addEventListener("click", function () {
   location.href = 'mypage.html';
+});
+main.addEventListener("click", function () {
+  location.href = 'mypage2.html';
+});
+insta.addEventListener("click", function () {
+  location.href = 'mypage2.html';
+});
+
+
+var change1 = document.querySelector("#change1");
+var change2 = document.querySelector("#change2");
+var ch1 = change1.style.backgroundColor;
+var ch2 = change2.style.backgroundColor;
+var count1=1;
+var count2=1;
+change1.addEventListener("click", function () {
+  console.log(ch1);
+  if (count1 == 1) {
+    change1.style.backgroundColor = "rgb(178,235,244)";
+    count1=0; //178 흰색
+  }
+  else {
+    change1.style.backgroundColor = "rgb(0,149,249)";
+    count1=1;
+  }
+});
+change2.addEventListener("click", function () {
+  if (count2 == 1) {
+    change2.style.backgroundColor = "rgb(178,235,244)";
+    count2=0; //178 흰색
+  }
+  else {
+    change2.style.backgroundColor = "rgb(0,149,249)";
+    count2=1;
+  }
 });
